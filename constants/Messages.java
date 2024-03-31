555c3151ac68505eba1bd666b903c1ef9e3f294f
Ej Sobrepeña <ej.sobrepena@tuni.fi>
2024-03-31 13:08:31 +0300

    Add Messages class for predefined application messages

---
+++ constants/Messages.java
@@ -0,0 +1,19 @@
+package constants;
+
+/**
+ * The Messages class provides a set of predefined messages that can be used
+ * throughout the application.
+ * It contains messages for game completion and error notification.
+ */
+public class Messages {
+
+    /**
+     * Message displayed when the game is successfully completed.
+     */
+    public static final String GAME_COMPLETE = "Congratulations, you have won! New Game?";
+
+    /**
+     * Message displayed when an error occurs.
+     */
+    public static final String ERROR = "An error has occurred.";
+}
