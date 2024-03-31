8cae9a450196e5a5916244b0b521245a9032074e
Ej Sobrepeña <ej.sobrepena@tuni.fi>
2024-03-31 13:11:25 +0300

    Introduce Rows enum to represent rows in a Sudoku square

---
+++ constants/Rows.java
@@ -0,0 +1,14 @@
+package constants;
+
+/**
+ * The Rows enum represents the rows in a 3x3 square of a Sudoku game.
+ *
+ * - `TOP`: The top row in a square.
+ * - `MIDDLE`: The middle row in a square.
+ * - `BOTTOM`: The bottom row in a square.
+ */
+public enum Rows {
+    TOP,
+    MIDDLE,
+    BOTTOM
+}
